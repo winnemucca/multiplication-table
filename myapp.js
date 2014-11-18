e1c2e7d5a27a9b02297ef949f98da6ef4ae959ef
Steven Kauyedauty <skauyedauty@gmail.com>
2014-11-18 14:48:26 -0700

    added some features

--- myapp.js
+++ myapp.js
@@ -1,22 +1,29 @@
 angular.module('myApp',[])
 	.controller('MultiplicationCtrl',function($scope,$attrs) {
 		function populateNumbers(x) {
-			var numbers - [];
+			var numbers = [];
 			for(var i=0; i<x; i++) {
 				numbers[i] = i +1;
 			};
 			return numbers;
 		}
+		// $scope.numbers = populateNumbers($scope.numberLimit);
+
+
+		$scope.compute = function(a,b) {
+			return a *b;
+		}
+		$scope.$watch('numberLimit', function(limit) {
+			$scope.numbers = populateNumbers(limit);
+		});
 		$scope.numberLimit = $attrs.initialNumberLimit || 10;
-		$scope.numbers = populateNumbers($scope.numberLimit);
 
-		
-		// $scope.compute = function(a,b) {
-		// 	return a *b;
-		// };
 	});
-
-
+	// var x = function(string) {
+	// 	return string;
+	// }
+	// console.log(x("What \nis \tlove?"));
+	// console.log(x("origin\\destination"));
 
 
 	// $attrs is key/value map of all the attributes present on the elent where the ng-controller was placed.
